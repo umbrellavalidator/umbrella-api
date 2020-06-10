63b204ae517d21874ba1ec4d5d961f4f49dceace
Luigi <luigi@umbrellavalidator.com>
2020-06-09 23:37:50 -0700

    Update template

--- index.js
+++ index.js
@@ -1,3 +1,95 @@
+/**
+ * Notes on API
+ * 1. use nouns in endpoint paths
+ * 2. name collections with plural nouns
+ * 3. nesting resources for hierarchical objects
+ * 4. handle errors gracefully and return standard error codes
+ * 5. allow filtering, sorting, and pagination
+ * 6. cache data to improve performance
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// 6.
+// an example of a in-memory cache. An alternative: redis
+const apicache = require('apicache');
+let cache = apicache.middleware;
+app.use(cache('5 minutes'));
+
+app.get('/validators', (req, res) => {
+    const validators = [];
+    // code to retrieve an validators...
+    res.json(validators);
+});
+
+app.post('/validators', (req, res) => {
+    // code to add a new validators...
+    res.json(req.body);
+});
+
+app.put('/validators/:id', (req, res) => {
+    const { id } = req.params;
+    // code to update an validators...
+    res.json(req.body);
+});
+
+app.delete('/validators/:id', (req, res) => {
+    const { id } = req.params;
+    // code to delete an validators...
+    res.json({ deleted: id });
+});
+
+// 3.
+app.get('/validators/:validatorId/comments', (req, res) => {
+    const { validatorId } = req.params;
+    const comments = [];
+    // code to get comments by validatorId
+    res.json(comments);
+});
+
+// 4.
+const users = [
+    { email: 'abc@foo.com' }
+]
+app.post('/users', (req, res) => {
+    const { email } = req.body;
+    const userExists = users.find(u => u.email === email);
+    if (userExists) {
+        return res.status(400).json({ error: 'User already exists' })
+    }
+    res.json(req.body);
+});
+
+// 5.
+const delegation = [
+    { address: 'cosmos11', validator: 'cosmosvaloper1', amount: 10 },
+    { address: 'cosmos12', validator: 'cosmosvaloper1', amount: 20 },
+    { address: 'cosmos13', validator: 'cosmosvaloper2', amount: 30 },
+]
+
+app.get('/delegation', (req, res) => {
+    const { address, validator, amount } = req.query;
+    let results = [...delegation];
+    if (address) {
+        results = results.filter(r => r.address === address);
+    }
+
+    if (validator) {
+        results = results.filter(r => r.validator === validator);
+    }
+
+    if (amount) {
+        results = results.filter(r => +r.amount === +amount);
+    }
+    res.json(results);
+});
+
+app.listen(3000, () => console.log('server started'));
+
+/*
 const { exec } = require('child_process');
 const app = polka();  // Same as Express.js but might be faster.
 
@@ -46,3 +138,4 @@ app.get('/net_info', (req, res) => {
 app.get('/consensus_state', (req, res) => {
   res.end(consensusState());
 });
+*/
